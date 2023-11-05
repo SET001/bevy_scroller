b63fcc55e6e1eb426155fc32c8087442f7048198
SET001 <settydark@gmail.com>
2023-11-05 16:10:50 +0200

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 #![allow(clippy::type_complexity)]
 
-// mod linear_sprite_spawner;
-// mod poisson_sprite_spawner;
 mod generator;
 mod generators;
 mod plugin;
@@ -13,15 +11,3 @@ pub use plugin::ScrollerPlugin;
 pub use scroller::{
   Scroller, ScrollerBundle, ScrollerDirection, ScrollerInitialized, ScrollerItem, ScrollerSize,
 };
-
-// use scroller::{
-//   basic_scroller_init, bounded_scroller_init, scroller_on_items_added, ScrollerCameras,
-// };
-// // use linear_sprite_spawner::linear_sprite_spawner_init;
-// // use poisson_sprite_spawner::poisson_sprite_spawner;
-// // use scroller::scroller_init;
-
-// pub use scroller::{
-//   scroller_debug, scroller_delete_items, scroller_update, Scroller, ScrollerBundle, ScrollerItem,
-//   ScrollerTypeBasic, ScrollerTypeBounded,
-// };
